2627507771f259b31098df20c06046c198e3a65c
Thomas Culdaut <thomas@wildcodeschool.fr>
2020-10-18 01:41:40 +0200

    linting graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
       'prettier/@typescript-eslint',
       'plugin:prettier/recommended'
     ],
-    plugins: ['react', '@typescript-eslint', 'jest'],
+    plugins: ['react', '@typescript-eslint', 'jest', 'graphql'],
     env: {
       browser: true,
       es6: true,
@@ -37,5 +37,66 @@ module.exports = {
           endOfLine: 'auto',
         },
       ],
+      "graphql/template-strings": ['error', {
+        // Import default settings for your GraphQL client. Supported values:
+        // 'apollo', 'relay', 'lokka', 'fraql', 'literal'
+        env: 'apollo',
+  
+        // Import your schema JSON here
+        // schemaJson: require('./schema.json'),
+  
+        // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
+        // schemaJsonFilepath: path.resolve(__dirname, './schema.json'),
+  
+        // OR provide the schema in the Schema Language format
+        schemaString: `type Spell {
+          id: Int!
+          name: String!
+          description: String!
+          range: String
+          components: String
+          duration: String
+          castingTime: String
+          target: String
+          spellResistance: String
+          savingThrow: String
+        }
+        
+        type Spellbook {
+          id: String!
+          name: String!
+          spells: [Spell]
+        }
+        
+        input InputSpell {
+          name: String
+        }
+        
+        input InputSpellbook {
+          name: String
+        }
+        
+        input InputSpellToBook {
+          bookId: String!
+          spellId: Int!
+        }
+        
+        type ResolveType {
+          done: Boolean
+        }
+        
+        type Query {
+          getSpells(name: String): [Spell]
+          allSpellbooks: [Spellbook!]
+        }
+        
+        type Mutation {
+          addSpellbook(input: InputSpellbook!): Spellbook!
+          addSpellToBook(input: InputSpellToBook): Spell!
+        }
+        `,
+  
+        // tagName is gql by default
+      }]
     },
   };
